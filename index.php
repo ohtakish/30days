150cfdb2c18b33dc8ce70074b34323aaf99a2cd1
大滝昇平 <oh5.dartsman@gmail.com>
2019-06-06 04:09:20 +0900

    リンク・アイキャッチ・タイトルなどを動的に表示するよう変更
    ループを動的に！

--- index.php
+++ index.php
@@ -63,21 +63,36 @@ if (have_posts() ) :
         ?>
 
 <!-- entry-item -->
-<a href="#" class="entry-item">
+<a href="<?php the_permalink(); //記事のリンクを表示 ?>" class="entry-item">
     <!-- entry-item-img -->
     <div class="entry-item-img">
-        <img src="<?php echo get_template_directory_uri() ?>/img/entry1.png" alt="">
+        <?php
+        if (has_post_thumbnail() ) {
+            // アイキャッチ画像が設定されてれば大サイズで表示
+            the_post_thumbnail('large');
+        } else {
+            // なければnoimage画像をデフォルトで表示
+            echo '<img src="' . esc_url(get_template_directory_uri()) . '/img/noimg.png" alt="">';
+        }
+        ?>
     </div><!-- /entry-item-img -->
 
     <!-- entry-item-body -->
     <div class="entry-item-body">
         <div class="entry-item-meta">
-            <div class="entry-item-tag">カテゴリ名</div><!-- /entry-item-tag -->
-            <time class="entry-item-published" datetime="2019-01-01">2019/1/1</time><!-- /entry-item-published -->
+        <?php
+        // カテゴリー１つ目の名前を表示
+        $category = get_the_category();
+        if ($category[0] ) {
+            echo '<div class="entry-item-tag">' . $category[0]->cat_name . '</div><!-- /entry-item-tag -->';
+        }
+        ?>
+            <!-- 公開日時を動的に表示する -->
+            <time class="entry-item-published" datetime="<?php the_time('c'); ?>"><?php the_time('Y/n/j'); ?></time><!-- /entry-item-published -->
         </div><!-- /entry-item-meta -->
-        <h2 class="entry-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</h2><!-- /entry-item-title -->
+        <h2 class="entry-item-title"><?php the_title(); //タイトルを表示 ?></h2><!-- /entry-item-title -->
         <div class="entry-item-excerpt">
-            <p>文章の一部が入ります文章の一部が入ります文章の一部が入ります文章の一部が入ります文章の一部が入…</p>
+            <?php the_excerpt(); //抜粋を表示 ?>
         </div><!-- /entry-item-excerpt -->
     </div><!-- /entry-item-body -->
 </a><!-- /entry-item -->
