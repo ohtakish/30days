670d6094fd91361c3fcaf1582813f76fecbf30bb
大滝昇平 <oh5.dartsman@gmail.com>
2019-06-06 01:49:29 +0900

    style.cssとfunctions.phpを設定。テーマとして認識させる。

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * テーマのセットアップ
+ * https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/add_theme_support#HTML5
+ */
+function my_setup() {
+	add_theme_support( 'post-thumbnails' ); /* アイキャッチ画像を有効化 */
+	add_theme_support( 'automatic-feed-links' ); /* 投稿とコメントのRSSフィードのリンクを有効化 */
+	add_theme_support( 'title-tag' ); /* タイトルタグ自動生成 */
+	add_theme_support(
+		'html5',
+		array( /* HTML5でマークアップ */
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		)
+	);
+}
+add_action( 'after_setup_theme', 'my_setup' );
+// セットアップの書き方の型
+// function custom_theme_setup() {
+// add_theme_support( $feature, $arguments );
+// }
+// add_action( 'after_setup_theme', 'custom_theme_setup' );
