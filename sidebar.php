46fd1c905995d005f599fd5dc5ff25bcd7365390
大滝昇平 <oh5.dartsman@gmail.com>
2019-06-06 02:59:35 +0900

    【演習】sidebarをheader・footerと同じ要領で分割

---
+++ sidebar.php
@@ -0,0 +1,143 @@
+<!-- secondary -->
+<aside id="secondary">
+
+<!-- widget -->
+<div class="widget widget_text widget_custom_html">
+<div class="widget-title">プロフィール</div>
+
+<div class="wprofile">
+<div class="wprofile-img"><img src="<?php echo get_template_directory_uri() ?>/img/profile.png" alt=""></div>
+<div class="wprofile-content">
+    <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+</div>
+<!-- /wprofile-content -->
+<nav class="wprofile-sns">
+    <div class="wprofile-sns-item m_twitter"><a href="" rel="noopener noreferrer" target="_blank"><i class="fab fa-twitter"></i></a></div>
+    <div class="wprofile-sns-item m_facebook"><a href="" rel="noopener noreferrer" target="_blank"><i class="fab fa-facebook-f"></i></a></div>
+    <div class="wprofile-sns-item m_instagram"><a href="" rel="noopener noreferrer" target="_blank"><i class="fab fa-instagram"></i></a></div>
+</nav>
+</div><!-- /wprofile -->
+</div><!-- /widget -->
+
+
+<!-- widget -->
+<div class="widget widget_search">
+<div class="widget-title">検索</div>
+<!-- search-form -->
+<form method="get" class="search-form" action="#">
+    <input type="search" class="search-field" value="" placeholder="キーワード" name="s" id="s">
+    <button type="submit" class="search-submit"><i class="fas fa-search"></i></button>
+</form><!-- /search-form -->
+</div><!-- /widget -->
+
+
+<!-- widget -->
+<div class="widget widget_popular">
+<div class="widget-title">人気記事</div>
+
+<div class="wpost-items m_ranking">
+
+<!-- wpost-item -->
+<a class="wpost-item" href="#">
+    <div class="wpost-item-img"><img src="<?php echo get_template_directory_uri() ?>/img/entry2.png" alt=""></div>
+    <div class="wpost-item-body">
+        <div class="wpost-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div>
+    </div><!-- /wpost-item-body -->
+</a><!-- /wpost-item -->
+
+<!-- wpost-item -->
+<a class="wpost-item" href="#">
+    <div class="wpost-item-img"><img src="<?php echo get_template_directory_uri() ?>/img/entry1.png" alt=""></div>
+    <div class="wpost-item-body">
+        <div class="wpost-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div>
+    </div><!-- /wpost-item-body -->
+</a><!-- /wpost-item -->
+
+<!-- wpost-item -->
+<a class="wpost-item" href="#">
+    <div class="wpost-item-img"><img src="<?php echo get_template_directory_uri() ?>/img/entry3.png" alt=""></div>
+    <div class="wpost-item-body">
+        <div class="wpost-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div>
+    </div><!-- /wpost-item-body -->
+</a><!-- /wpost-item -->
+
+<!-- wpost-item -->
+<a class="wpost-item" href="#">
+    <div class="wpost-item-img"><img src="<?php echo get_template_directory_uri() ?>/img/entry4.png" alt=""></div>
+    <div class="wpost-item-body">
+        <div class="wpost-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div>
+    </div><!-- /wpost-item-body -->
+</a><!-- /wpost-item -->
+
+<!-- wpost-item -->
+<a class="wpost-item" href="#">
+    <div class="wpost-item-img"><img src="<?php echo get_template_directory_uri() ?>/img/entry5.png" alt=""></div>
+    <div class="wpost-item-body">
+        <div class="wpost-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div>
+    </div><!-- /wpost-item-body -->
+</a><!-- /wpost-item -->
+
+</div><!-- /wpost-items -->
+</div><!-- /widget -->
+
+
+
+<!-- widget -->
+<div class="widget widget_recent">
+<div class="widget-title">新着記事</div>
+
+<div class="wpost-items">
+
+<!-- wpost-item -->
+<a class="wpost-item" href="#">
+    <div class="wpost-item-img"><img src="<?php echo get_template_directory_uri() ?>/img/entry7.png" alt=""></div>
+    <div class="wpost-item-body">
+        <div class="wpost-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div>
+    </div><!-- /wpost-item-body -->
+</a><!-- /wpost-item -->
+
+<!-- wpost-item -->
+<a class="wpost-item" href="#">
+    <div class="wpost-item-img"><img src="<?php echo get_template_directory_uri() ?>/img/entry6.png" alt=""></div>
+    <div class="wpost-item-body">
+        <div class="wpost-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div>
+    </div><!-- /wpost-item-body -->
+</a><!-- /wpost-item -->
+
+<!-- wpost-item -->
+<a class="wpost-item" href="#">
+    <div class="wpost-item-img"><img src="<?php echo get_template_directory_uri() ?>/img/entry10.png" alt=""></div>
+    <div class="wpost-item-body">
+        <div class="wpost-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div>
+    </div><!-- /wpost-item-body -->
+</a><!-- /wpost-item -->
+
+<!-- wpost-item -->
+<a class="wpost-item" href="#">
+    <div class="wpost-item-img"><img src="<?php echo get_template_directory_uri() ?>/img/entry7.png" alt=""></div>
+    <div class="wpost-item-body">
+        <div class="wpost-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div>
+    </div><!-- /wpost-item-body -->
+</a><!-- /wpost-item -->
+
+<!-- wpost-item -->
+<a class="wpost-item" href="#">
+    <div class="wpost-item-img"><img src="<?php echo get_template_directory_uri() ?>/img/entry9.png" alt=""></div>
+    <div class="wpost-item-body">
+        <div class="wpost-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</div>
+    </div><!-- /wpost-item-body -->
+</a><!-- /wpost-item -->
+
+</div><!-- /wpost-items -->
+</div><!-- /widget -->
+
+<div class="widget widget_archive">
+<div class="widget-title">アーカイブ</div>
+<ul>
+    <li><a href="#">テキストテキストテキスト</a></li>
+    <li><a href="#">テキストテキストテキスト</a></li>
+    <li><a href="#">テキストテキストテキスト</a></li>
+</ul>
+</div><!-- /widget -->
+
+</aside><!-- secondary -->
