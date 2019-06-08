ab50d337add70df10414a024826c27427b8470a7
大滝昇平 <oh5.dartsman@gmail.com>
2019-06-08 23:18:26 +0900

    メニューを有効にするための記述をfunctions.phpに記述
    参考：https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/register_nav_menus

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * テーマのセットアップ
- * https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/add_theme_support#HTML5
+ * 参考：https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/add_theme_support#HTML5
  */
 function my_setup()
 {
@@ -30,7 +30,7 @@ add_action('after_setup_theme', 'my_setup');
 /**
  * CSSとJavaScriptの読み込み
  *
- * @codex https://wpdocs.osdn.jp/%E3%83%8A%E3%83%93%E3%82%B2%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%83%A1%E3%83%8B%E3%83%A5%E3%83%BC
+ * 参考：https://wpdocs.osdn.jp/%E3%83%8A%E3%83%93%E3%82%B2%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%83%A1%E3%83%8B%E3%83%A5%E3%83%BC
  */
 function my_script_init()
 {
@@ -39,3 +39,21 @@ function my_script_init()
     wp_enqueue_script('my', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
+
+
+/**
+ * メニューの登録
+ *
+ * 参考：https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/register_nav_menus
+ */
+function my_menu_init()
+{
+    register_nav_menus(
+        array(
+        'global'  => 'ヘッダーメニュー',
+        'footer'  => 'フッターメニュー',
+        'drawer'  => 'ドロワーメニュー',
+        )
+    );
+}
+add_action('init', 'my_menu_init');
